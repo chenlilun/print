0fd9b5f1363622bf4f0633f539011e35f7ce2a4f
chen <2501238913@qq.com>
2023-05-30 14:44:37 +0800

    444

--- src/main/java/com/example/print/print/Tcpclient.java
+++ src/main/java/com/example/print/print/Tcpclient.java
@@ -59,77 +59,21 @@ public class Tcpclient {
     }
 
     public  void sendData(DataOutputStream dos, PackageBox packageBox) {
-//        String dataStr =
-//                        "^XA\n" +
-//                        "^CWZ,E:SIMSUN.TTF\n" +
-//                        "^JMA^LL800^PW700^MD0^PR3^PON^LRN^LH0,0\n" +
-//                        "^CI28" +
-//                        "^FO192,64^GFA,16896,16896,00044,:Z64:\n" +
-//                        "^FO280,100\n" +
-//                        "^BQN,2,10\n" +
-//                        "^FDHM,B0400"+packageBox.getProductInfo().get(0).getNewProductCode()+"^FS\n" +
-//                        "^PQ1\n" +
-//                        "^FT210,708^A0B,39,38^FH\\^FD" + packageBox.spec + "^FS\n" +
-//                        "^FT265,708^A0B,39,38^FH\\^FD" + packageBox.getBatchNo() + "^FS\n" +
-//                        "^FT313,708^A0B,39,38^FH\\^FD" + packageBox.getGrade() + "^FS\n" +
-//                        "^FO382,600^AZB,48,48\n" + "^FD"+packageBox.getColor()+"^FS\n" +
-//                        "^FT367,708^A0B,39,38^FH\\^FD" + Integer.valueOf((int) packageBox.getProductInfo().get(0).getSilkCount()) + "^FS\n" +
-//                        "^FT467,708^A0B,39,38^FH\\^FD" + packageBox.getProductInfo().get(0).getNetWeight() + "KG" + "^FS\n" +
-//                        "^FT515,708^A0B,39,38^FH\\^FD" + packageBox.getProductInfo().get(0).getGrossWeight() + "KG" + "^FS\n" +
-//                        "^FT569,708^A0B,39,38^FH\\^FD" + getDateStr(packageBox.getProduceTime()) + "^FS\n" +
-//                        "^FT613,708^A0B,39,38^FH\\^FD" + packageBox.getProductInfo().get(0).getNewProductCode() + "^FS\n" +
-//                        "^PQ1,0,1,Y^XZ";
 
-//        String dataStr = "^XA\\n \n" +
-//                "^CWZ,E:\tSimHei.TTF\\n \n" +
-//                "^JMA^LL800^PW1100^MD0^PR3^PON^LRN^LH0,0\\n \n" +
-//                "^CI28 \n" +
-//                "^FO480,350\n" +
-//                "^BQN,2,15\n" +
-//                "^FDHM,B0400FDA201015XD020701DZ2B201^FS \n" +
-//                "^PQ1 \n" +
-//                "^FO188,140^AZN,72,72 ^FD涤纶预取向丝POY^FS\n" +
-//                "^FT158,216^A0N,39,38^FH\\^FD250dex/25f^FS\n" +
-//                "^FT158,267^A0N,39,38^FH\\^FDXC5555400CD^FS\n" +
-//                "^FT158,323^A0N,39,38^FH\\^FDAAA^FS\n" +
-//                "^FT158,373^A0N,39,38^FH\\^FD99^FS\n" +
-//                "^FO112,390^AZN,48,48 ^FD 黄蓝条^FS\n" +
-//                "^FT158,471^A0N,39,38^FH\\^FD720.23KG^FS\n" +
-//                "^FT158,526^A0N,39,38^FH\\^FD726.23KG^FS\n" +
-//                "^FT158,578^A0N,39,38^FH\\^FD2022-12-23^FS\n" +
-//                "^FT158,630^A0N,39,38^FH\\^FDA201015XD020701DZ2B201^FS\n" +
-//                "^FO462,530^AZN,72,72 ^FD 甲001^FS\n" +
-//                "^PQ1,0,1,Y^XZ";
 
         try {
             System.out.println("=========================================");
-            ;
-/*
-            String dataStr111 = "^XA" +
-                    "^CWZ,E:SimHei.TTF" +
-                    "^JMA^LL800^PW1100^MD0^PR3^PON^LRN^LH0,0" +
-                    "^CI26" +
-                    "^FO480,200" +
-                    "^BQN,2,10" +
-                    "^FDHM,B0400FDA201015XD020701DZ2B201^FS" +
-                    "^PQ1" +
-                    "^FO188,140^AZN,72,72 ^FD涤纶预取向丝POY^FS" +
-                    "^FT158,236^A0N,39,38^FH^FD250dex/25f^FS" +
-                    "^FT158,287^A0N,39,38^FH^FDXC5555400CD^FS" +
-                    "^FT158,343^A0N,39,38^FH^FDAAA^FS" +
-                    "^FT158,393^A0N,39,38^FH^FD99^FS" +
-                    "^FO112,410^AZN,48,48 ^FD 黄蓝条^FS" +
-                    "^FT158,491^A0N,39,38^FH^FD720.23KG^FS" +
-                    "^FT158,546^A0N,39,38^FH^FD726.23KG^FS" +
-                    "^FT158,598^A0N,39,38^FH^FD2022-12-23^FS" +
-                    "^FT158,650^A0N,39,38^FH^FDA201015XD020701DZ2B201^FS" +
-                    "^FO462,530^AZN,72,72 ^FD 甲001^FS" +
-                    "^PQ1,0,1,Y^XZ";*/
+
 
 
             String product = "POY".equals(packageBox.getProductName()) ? "涤纶预取向丝POY" : "涤纶牵伸丝FDY";
-            String realDatas = "^XA^CWZ,E:SimHei.TTF^JMA^LL1182^PW1100^MD0^PR3^PON^LRN^LH0,0^CI28^FO709,296^BQN,2,20^FDLA," + packageBox.getProductInfo().get(0).getNewProductCode() +
-                    "^FS^PQ1^FO278,180^AZN,106,96^FD" +
+
+            String qrcode = "^XA^CWZ,E:SIMSUN.TTF^JMA^LL1182^PW1100^MD0^PR3^PON^LRN^LH0,0^CI28^FO459,196^BQN,2,20^FDLA," + packageBox.getProductInfo().get(0).getNewProductCode()
+                 +"^FS";
+            String print = app.print(qrcode);
+            print += print +
+
+            "^PQ1^FO278,180^AZN,106,96^FD" +
                     product
                     + "^FS^FT240,349^A0N,63,56^FH^FD" +
                     packageBox.getSpec() +
@@ -149,10 +93,10 @@ public class Tcpclient {
                     getDateStr(packageBox.getProduceTime()) +
                     "^FS^FT240,950^A0N,58,56^FH^FD" +
                     packageBox.getProductInfo().get(0).getNewProductCode() +
-                    "^FS^FO700,783^AZN,106,106^FD甲001^FS^PQ1,0,1,Y^XZ";
-
-
-            dos.writeUTF(app.print(realDatas));
+                    "^FS^FO700,783^AZN,106,106^FD甲001^FS"+
+                    "^PQ1,0,1,Y^XZ";
+            dos.writeUTF(print);
+//            dos.writeUTF(dataStr111);
             dos.flush();
 
           /*  String test = "^XA\n" +
